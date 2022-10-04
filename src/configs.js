e05284c23381f7172737538e97cda79db37a1ae0
Radi <evaradi18@gmail.com>
2022-10-04 16:00:01 +0100

    init

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,6 @@
 //export const _API_URL = "https://elfrique-proj.herokuapp.com/api/v1/";
-export const _API_URL = "http://127.0.0.1:3000/api/v1/";
-// export const _API_URL = "https://elfrique-backend.herokuapp.com/api/v1/";
+// export const _API_URL = "http://127.0.0.1:3000/api/v1/";
+export const _API_URL = "https://elfrique-backend2.herokuapp.com/api/v1/";
 
 
 export const _BASE_URL = "https://sandbox.elfrique.com/";
